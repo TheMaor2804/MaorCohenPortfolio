a93e675ab2bca2129d3da730ccbb1475cdb42405
TheMaor2804 <mhgameing@Gmail.com>
2024-12-02 16:52:19 +0200

    remove Rich Text Editor files and related code

--- projects/Javascript Projects/Rich Text Editor/src/main.js
+++
@@ -1,8 +0,0 @@
-import createEditor from "./DOM/domManipulator.js";
-let error = createEditor("editor");
-if (error !== "") {
-    console.log(error);
-}
-else {
-    console.log("Editor created successfully");
-}
--- projects/Javascript Projects/Rich Text Editor/src/main.ts
+++
@@ -1,9 +0,0 @@
-import createEditor from "./DOM/domManipulator.js";
-
-let error: string = createEditor("editor");
-
-if (error !== "") {
-    console.log(error);
-} else {
-    console.log("Editor created successfully");
-}
